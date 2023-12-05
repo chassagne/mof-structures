a9a0b691a010a18c17db7830f2a23d1e027567a4
Titouan <41014275+chassagne@users.noreply.github.com>
2023-12-05 14:13:07 +0100

    Attempt 3 at fixing iOS bug

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function openPdf() {
+function updatePdfLink() {
     var dropdown = document.getElementById("countryDropdown");
     var selectedValue = dropdown.value;
+    var link = document.getElementById("pdfLink");
 
-    if (selectedValue && selectedValue !== 'Select a country') {
-        if (confirm('Do you want to open the PDF?')) {
-            window.open(selectedValue, '_blank');
-        }
+    if (selectedValue) {
+        link.href = selectedValue;
+        link.style.display = 'inline';
+    } else {
+        link.style.display = 'none';
     }
 }
 
-document.getElementById("countryDropdown").onchange = openPdf;
+document.getElementById("countryDropdown").onchange = updatePdfLink;
