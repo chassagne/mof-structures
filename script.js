1f84c48d710dfe8b4b089160c493bae088e09e6b
Titouan <41014275+chassagne@users.noreply.github.com>
2023-12-05 13:46:43 +0100

    Attempt to fix iOS bug

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function openPdf() {
     var dropdown = document.getElementById("countryDropdown");
     var selectedValue = dropdown.value;
 
-    if (selectedValue) {
+    if (selectedValue && selectedValue !== 'Select a country') {
         window.open(selectedValue, '_blank');
     }
 }
+
+// Attach the openPdf function to the 'blur' event as well
+document.getElementById("countryDropdown").onblur = openPdf;
